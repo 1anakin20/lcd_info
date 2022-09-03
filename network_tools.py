1c0852341883f72cfbbb7f0d66b451c74f8544f1
1anakin20 <tomasdaniel09@hotmail.com>
2022-09-03 09:53:20 -0400

    Remove create_network()

--- network_tools.py
+++ network_tools.py
@@ -1,33 +1,5 @@
-# Shows the IP on the i2c lcd screen connected
 import socket
 import os
-import dbus
-
-
-# This does not work
-def create_network(ssid, password, interface='wlan0'):
-    bus = dbus.SystemBus()
-    wpas_obj = bus.get_object('fi.w1.wpa_supplicant1',
-                            '/fi/w1/wpa_supplicant1')
-
-    wpas = dbus.Interface(wpas_obj, 'fi.w1.wpa_supplicant1')
-    path = wpas.CreateInterface({'Ifname': interface})
-    if_obj = bus.get_object('fi.w1.wpa_supplicant1', path)
-    path = if_obj.AddNetwork({
-    'ssid':ssid,
-    'mode':1,
-    'frequency':2412,
-    'proto':'WPA',
-    'key_mgmt':'NONE',
-    'pairwise':'NONE',
-    'group':'TKIP',
-    'psk':password,
-    }, dbus_interface='fi.w1.wpa_supplicant1.Interface')
-
-    network = bus.get_object('fi.w1.wpa_supplicant1', path)
-    network.Set('fi.w1.wpa_supplicant1.Network', 'Enabled', True, dbus_interface=dbus.PROPERTIES_IFACE)
-    print(network.GetAll('fi.w1.wpa_supplicant1.Network', dbus_interface=dbus.PROPERTIES_IFACE))
-
 
 def get_network_ssid():
     return os.popen('sudo iwgetid -r').read()
